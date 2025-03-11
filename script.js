2c06d93b7eb8c1198c6bbe5664e78ac06b686f4a
TishkaGit <yatelo@vk.com>
2025-03-11 14:44:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -201,7 +201,7 @@ function getDirection(azimuth) {
 document.getElementById("fetchData").addEventListener("click", async () => {
     const apiUrl = "https://local-business-data.p.rapidapi.com/search";
     const headers = {
-        "x-rapidapi-key": "992552b12amsh319a18af82ce816p18154djsnb", // Новый API-ключ
+        "x-rapidapi-key": "658fcc2465mshe571567a1275df0p1bf12ajsnf576353bbc2f", // Новый API-ключ
         "x-rapidapi-host": "local-business-data.p.rapidapi.com"
     };
 
